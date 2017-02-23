66bb4e931a97d431bde3430588b51701c87375c5
大白萝 <gamhunter@163.com>
2017-02-23 12:05:26 +0800

    完成色子功能
    解决了多个色子的动画问题，设置了样式

--- pages/dices/dices.js
+++ pages/dices/dices.js
@@ -1,7 +1,8 @@
 // pages/dice/dices.js
 Page({
   data: {
-    diceCount: 4,
+    diceCount: 1,
+    dicesData:[]
   },
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
@@ -14,7 +15,10 @@ Page({
       6: "134679"
     };
     this.timer = null;
-    this.setDicesData(0);
+    this.animation = wx.createAnimation({
+      duration: 400,
+      timingFunction: 'linear',
+    });
   },
   onReady: function () {
     // 页面渲染完成
@@ -39,82 +43,79 @@ Page({
   },
 
   // 产生色子动画
-  createAnim: function (left,top) {
-    var diceAnim = wx.createAnimation({
-      duration: 400,
-      timingFunction: 'linear',
-    });
-    // 色子移出屏幕
-    diceAnim.top("-200rpx").left("-200rpx").step();
+  createAnim: function (left, top) {
     // 色子移入屏幕
-    var destinationX = 0,
-      destinationY = 0;
-    diceAnim.top(top+"rpx").left(left+"rpx").rotate(180).step({ duration: 1000, timingFunction: "ease", delay: 500 });
-    return diceAnim.export();
+    this.animation.top(top + "rpx").left(left + "rpx").rotate(Math.random()*180).step({ duration: 1000, timingFunction: "ease-out" });
+    return this.animation.export();
   },
 
   // 产生色子移动终点位置
   createDicesPos: function () {
     var dicesPos = [];
     // 色子位置判断
-    function judgePos(l,t){
-      for(var j = 0; j < dicesPos.length; j++){
+    function judgePos(l, t) {
+      for (var j = 0; j < dicesPos.length; j++) {
         // 判断新产生的色子位置是否与之前产生的色子位置重叠
-        if((dicesPos[j].left<l && l<dicesPos[j].left+146) && (dicesPos[j].top<t && t<dicesPos[j].top+146)){
+        if ((dicesPos[j].left-146 < l && l < dicesPos[j].left + 146) && (dicesPos[j].top-146 < t && t < dicesPos[j].top + 146)) {
           return false;
         }
       }
       return true;
     }
-    for(var i = 0; i < this.data.diceCount; i++){
-      var posData = {};
-      do{
+    for (var i = 0; i < this.data.diceCount; i++) {
+      var posData = {},
+          left = 0,
+          top = 0;
+      do {
         // 随机产生色子的可能位置
-        posData.left = Math.round(Math.random()*600); // 0~600,根据色子区域和色子的大小计算得出
-        posData.top = Math.round(Math.random()*550); // 0~550,根据色子区域和色子的大小计算得出
-      }while(!judgePos(posData.left,posData.top));
-       dicesPos.push(posData);
+        left = Math.round(Math.random() * 600); // 0~600,根据色子区域和色子的大小计算得出
+        top = Math.round(Math.random() * 550); // 0~550,根据色子区域和色子的大小计算得出
+      } while (!judgePos(left,top));
+      posData.left = left;
+      posData.top = top;
+      dicesPos.push(posData);
     }
     return dicesPos;
   },
 
   // 设置色子数据
-  setDicesData:function(diceCount){
+  setDicesData: function (diceCount) {
     var dicesData = [];
 
-    // 色子是否显示和色子动画数据
+    // 色子动画数据
     var dicesPos = this.createDicesPos(); // 所有色子的位置数据
-
-    for(var i = 0; i<9; i++){
+    for (var i = 0; i < diceCount; i++) {
       var diceData = {};
-      if(i < diceCount){
-        diceData.hidden = false;
-        diceData.anim = this.createAnim(dicesPos[i].left,dicesPos[i].left);
-      }else{
-        diceData.hidden = true;
-        diceData.anim = null;
-      }
+      diceData.anim = this.createAnim(dicesPos[i].left, dicesPos[i].top);
+      diceData.dots = this.createDotData();
       dicesData.push(diceData);
-    };
-    this.setData({dicesData:dicesData});
-
-    //色子点数数据
-    var that = this;
-    this.timer=setTimeout(function(){
-
-      for(var j = 0; j < diceCount; j++){
-        dicesData[j].dots = that.createDotData();
-      }
-      that.setData({
-        dicesData:dicesData
-      })
-    },400);
-
+    }
+    this.setData({ dicesData: dicesData });
+    
   },
 
   // 摇色子
   onRollTap: function () {
-    this.setDicesData(this.data.diceCount);
+    // 设置色子移出动画
+    var newData = this.data.dicesData;
+    if(newData.length < this.data.diceCount){
+      for(var i = 0; i < this.data.diceCount;i++){
+        var data = {};
+        newData.push(data);
+      }
+    }
+    for (var i = 0; i < newData.length; i++) {
+      this.animation.left("-233rpx").top("123rpx").rotate(-180).step();
+      newData[i].anim = this.animation.export();
+      this.setData({ dicesData: newData });
+    }
+    
+    var that = this;
+    this.timer = setTimeout(function(){
+      // 色子改变点数并移入屏幕
+      that.setDicesData(that.data.diceCount);
+    },1400)
+    
   },
 
   // 减少色子数量
@@ -128,7 +129,7 @@ Page({
 
   // 增加色子数量
   addDice: function () {
-    if (this.data.diceCount < 10) {
+    if (this.data.diceCount < 9) {
       this.setData({
         diceCount: this.data.diceCount + 1
       })
