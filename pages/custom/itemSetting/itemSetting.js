3ff50a13068fe94b62d37722e92e8142c671c1d2
大白萝 <gamhunter@163.com>
2017-02-24 11:05:19 +0800

    90%
    1.实现进入自定义项目界面读取之前设定的信息，
    2.增加项目按钮功能和移除项目按钮功能的完善

--- pages/custom/itemSetting/itemSetting.js
+++ pages/custom/itemSetting/itemSetting.js
@@ -1,32 +1,36 @@
 // pages/custom/itemSetting/itemSetting.js
 Page({
-  data:{
-    itemCount:0,
+  data: {
   },
-  onLoad:function(options){
+  onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
-    this.itemsData = [];
-  },
-  addItem:function(){
+    this.itemsData = wx.getStorageSync('itemsData') || [];
     this.setData({
-      itemCount:this.data.itemCount+1,
+      itemsData: this.itemsData
     })
   },
-  removeItem:function(event){
-    var newCout = this.data.itemCount-1;
+  addItem: function () {
+    this.itemsData.push("");
     this.setData({
-      itemCount:newCout
-    })
-    var idx = event.currentTarget.dataset.idx;
-    for(var i = idx;i<this.data.itemCount;i++){
+      itemsData: this.itemsData
+    });
 
-    }
+    this.setData({
+      itemsData: this.itemsData,
+      scrollTop: this.data.itemsData.length * 1000
+    });
   },
-  saveItem:function(event){
-    var item = event.detail.value;
-        this.itemsData.push(item);
+  removeItem: function (event) {
+    var idx = event.currentTarget.dataset.idx;
+    this.itemsData.splice(idx, 1);
+    this.setData({
+      itemsData: this.itemsData
+    })
   },
-  saveItems:function(){
+  saveItem: function (event) {
+    this.itemsData[this.itemsData.length - 1] = event.detail.value;
+  },
+  saveItems: function () {
     // 保存项目数据
     wx.setStorageSync('itemsData', this.itemsData);
     // 退回开始页面
