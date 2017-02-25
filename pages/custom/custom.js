b29aa8d7edecbf7c9b3ae7a454e219c4d357b11a
大白萝 <gamhunter@163.com>
2017-02-25 17:05:51 +0800

    改变摇奖方式
    将点击开始，再点击停止的抽奖方式改为只需点击一次的摇奖方式

--- pages/custom/custom.js
+++ pages/custom/custom.js
@@ -1,50 +1,62 @@
 // pages/custom/custom.js
 Page({
-  data:{
-    displayItem:"设置",
-    isRolling:false,
-    noData:true
+  data: {
+    displayItem: "设置",
+    noData: true
   },
-  onLoad:function(options){
+  onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
-    this.timer=null;
+    this.timer = null;
+    this.t = 50;
+    this.count = 0;
   },
-  onShow:function(){
+  onShow: function () {
     // 若没有设置过项目，或清空了项目，则隐藏开始按钮
-    if(wx.getStorageSync('itemsData')&&wx.getStorageSync('itemsData').length>0){
+    if (wx.getStorageSync('itemsData') && wx.getStorageSync('itemsData').length > 0) {
       this.setData({
-        noData:false
+        noData: false
       })
-    }else{
+    } else {
       this.setData({
-        displayItem:"设置",
-        noData:true
+        displayItem: "设置",
+        noData: true
       })
     }
   },
-  setItem:function(){
+  onHide: function () {
+    // 页面隐藏
+    clearInterval(this.timer);
+    this.t = 50;
+    this.count = 0;
+  },
+  setItem: function () {
     wx.navigateTo({
       url: 'itemSetting/itemSetting'
     })
   },
-  start:function(){
-    var that=this;
-    var itemsData=wx.getStorageSync('itemsData');
-    this.setData({
-      isRolling:true
-    })
-    clearInterval(this.timer);
-    this.timer=setInterval(function(){
-       var idx = Math.floor(Math.random()*itemsData.length);
-       that.setData({
-         displayItem:itemsData[idx].item
-       })
-    },100);
+
+  start: function () {
+    clearTimeout(this.timer);
+    this.roll();
   },
-  stop:function(){
-    clearInterval(this.timer);
+  roll: function () {
+    var itemsData = wx.getStorageSync('itemsData');
+    var idx = Math.floor(Math.random() * itemsData.length);
     this.setData({
-      isRolling:false
+      displayItem: itemsData[idx].item
     })
+    // 先每50毫秒变化一次，如此变化50次后变化间隔逐渐变大，直到停止
+    this.count++;
+    if (this.count < 50) {
+      this.timer = setTimeout(this.roll, 50);
+    } else {
+      this.t *= 1.5;
+      if (this.t < 2000) {
+        this.timer = setTimeout(this.roll, this.t);
+      } else {
+        this.t = 50;
+        this.count = 0;
+      }
+    }
   }
 })
--- pages/randomNum/randomNum.js
+++ pages/randomNum/randomNum.js
@@ -4,27 +4,20 @@ Page({
     num1:1,
     num2:100,
     result:"?",
-    btn:"开始",
-    btnBg:"#333"
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
       this.timer = null;
-  },
-  onReady:function(){
-    // 页面渲染完成
-  },
-  onShow:function(){
-    // 页面显示
+      this.t = 50; //数字正常变化的时间间隔
+      this.count = 0;// 数字正常变化的次数，大于这个次数后数字变化速度逐渐变慢
   },
   onHide:function(){
     // 页面隐藏
     clearInterval(this.timer);
+    this.t= 50;
+    this.count = 0;
   },
-  onUnload:function(){
-    // 页面关闭
-    clearInterval(this.timer);
-  },
+  
 
   setNum1:function(event){
     var num = parseInt(event.detail.value);
@@ -40,18 +33,23 @@ Page({
     })
   },
 
-  start:function(event){
-    if(this.data.btn == "开始"){
-      this.timer=setInterval(this.roll,100);
-      this.setData({
-        btn:"停止"
-      })
-    }else{
-      clearInterval(this.timer);
-      this.setData({
-        btn:"开始"
-      })
-    }
+  // start:function(event){
+  //   if(this.data.btn == "开始"){
+  //     this.timer=setInterval(this.roll,100);
+  //     this.setData({
+  //       btn:"停止"
+  //     })
+  //   }else{
+  //     clearInterval(this.timer);
+  //     this.setData({
+  //       btn:"开始"
+  //     })
+  //   }
+  // },
+
+  start:function(){
+    clearTimeout(this.timer);
+    this.roll();
   },
 
   roll:function(){
@@ -68,5 +66,18 @@ Page({
     this.setData({
       result:result
     })
+    // 让数字先每50毫秒变化一次，如此变化50次后变化间隔逐渐变大，直到停止
+    this.count++;
+    if(this.count < 50){
+      this.timer = setTimeout(this.roll,50);
+    }else{
+      this.t *= 1.5;
+      if(this.t < 2000){
+        this.timer = setTimeout(this.roll,this.t);
+      }else{
+        this.t = 50;
+        this.count = 0;
+      }
+    }
   }
 })
